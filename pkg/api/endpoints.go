2eab51d0add93cb4e4a18b72dfcadaf8096ed8e1
wellyblobby <tomrawc@gmail.com>
2026-04-08 01:39:54 +0400

    add pkg/api/endpoints.go

---
+++ pkg/api/endpoints.go
@@ -0,0 +1,101 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) Ben Coleman, 2022
+// Licensed under the MIT License.
+//
+// Optional extra endpoints you may want to add to your API
+// ----------------------------------------------------------------------------
+
+package api
+
+import (
+	"log"
+	"net/http"
+	"runtime"
+
+	"github.com/elastic/go-sysinfo"
+	"github.com/go-chi/chi/v5"
+	metrics "github.com/m8as/go-chi-metrics"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type Status struct {
+	Service      string `json:"service"`
+	Healthy      bool   `json:"healthy"`
+	Version      string `json:"version"`
+	BuildInfo    string `json:"buildInfo"`
+	Hostname     string `json:"hostname"`
+	OS           string `json:"os"`
+	Architecture string `json:"architecture"`
+	CPUCount     int    `json:"cpuCount"`
+	GoVersion    string `json:"goVersion"`
+	ClientAddr   string `json:"clientAddr"`
+	ServerHost   string `json:"serverHost"`
+	Uptime       string `json:"uptime"`
+}
+
+// AddOKEndpoint adds an endpoint that respond 200 when hitting it
+func (b *Base) AddOKEndpoint(r chi.Router, path string) {
+	log.Printf("### 🍏 API: 200 OK endpoint at: %s", "/"+path)
+
+	r.Get("/"+path, func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		b.ReturnText(w, "OK")
+	})
+}
+
+// AddMetrics adds Prometheus metrics to the router
+func (b *Base) AddMetricsEndpoint(r chi.Router, path string) {
+	log.Printf("### 🔬 API: metrics endpoint at: %s", "/"+path)
+
+	r.Use(metrics.SetRequestDuration)
+	r.Use(metrics.IncRequestCount)
+	r.Handle("/"+path, promhttp.Handler())
+}
+
+// AddHealth adds a health check endpoint to the API
+// preCheck is an optional function that can be used to perform a pre-check to set the Healthy status.
+func (b *Base) AddHealthEndpoint(r chi.Router, path string, preCheck func() bool) {
+	log.Printf("### 💚 API: health endpoint at: %s", "/"+path)
+
+	r.HandleFunc("/"+path, func(w http.ResponseWriter, r *http.Request) {
+		if preCheck != nil {
+			b.Healthy = preCheck()
+		}
+
+		if b.Healthy {
+			w.WriteHeader(http.StatusOK)
+			b.ReturnText(w, "OK: Service is healthy")
+		} else {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			b.ReturnText(w, "Error: Service is not healthy")
+		}
+	})
+}
+
+// AddStatus adds a status & info endpoint to the API
+func (b *Base) AddStatusEndpoint(r chi.Router, path string) {
+	log.Printf("### 🔮 API: status endpoint at: %s", "/"+path)
+
+	r.HandleFunc("/"+path, func(w http.ResponseWriter, r *http.Request) {
+		host, _ := sysinfo.Host()
+		host.Info().Uptime()
+
+		status := Status{
+			Service:      b.ServiceName,
+			Healthy:      b.Healthy,
+			Version:      b.Version,
+			BuildInfo:    b.BuildInfo,
+			Hostname:     host.Info().Hostname,
+			GoVersion:    runtime.Version(),
+			OS:           runtime.GOOS,
+			Architecture: runtime.GOARCH,
+			CPUCount:     runtime.NumCPU(),
+			ClientAddr:   r.RemoteAddr,
+			ServerHost:   r.Host,
+			Uptime:       host.Info().Uptime().String(),
+		}
+
+		b.ReturnJSON(w, status)
+	})
+}
